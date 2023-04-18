994a308e6c19f8637e2d757bebde30645f096c17
Ankit mukherjee <ankitmukherjee0010@gmail.com>
2023-04-18 16:47:31 +0530

    Task controller and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 8000;
+const {errorHandler} = require('./middleware/errorMiddleware')
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
